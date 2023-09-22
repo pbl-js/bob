ba1b3735876d7de6fce3db56e6d9570c5c429b8f
pablo <pablo@macbook-pro-pawe.home>
2023-09-22 19:34:15 +0200

    Add jest

---
+++ express/tests/createDataField.test.ts
@@ -0,0 +1,11 @@
+describe('Math functions', () => {
+  it('should multiply 5 by 3', () => {
+    const result = 15;
+    expect(result).toEqual(15);
+  });
+
+  it('should add 5 by 3', () => {
+    const result = 9;
+    expect(result).toEqual(8);
+  });
+});
