4a495395783cb49d75175fe1caba24078212007f
pablo <pablo@macbook-pro-pawe.home>
2023-09-24 00:23:42 +0200

    Add example of mongo data insertion

--- express/index.ts
+++ express/index.ts
@@ -1,14 +1,42 @@
 import 'dotenv/config';
 import express from 'express';
-import { run } from './src/packages/db/mongo';
+import { client, run } from './src/packages/db/mongo';
 
-function main() {
+async function main() {
   const app = express();
   const port = process.env.PORT;
 
-  run().catch(console.dir);
+  await run().catch(console.dir);
 
-  app.get('/', (req, res) => {
+  app.get('/', async (req, res) => {
+    await client.connect();
+    const myDB = client.db('mongotron');
+    const modelSchemaCollection = myDB.collection('model-schema');
+    const doc = {
+      name: 'page',
+      description: 'Example something',
+      fields: [
+        {
+          name: 'test string field',
+          type: 'string',
+          defaultValue: 'somekingofdefaultvalue',
+        },
+        {
+          name: 'test object field',
+          type: 'object',
+          defaultValue: null,
+          fields: [
+            {
+              name: 'test string field',
+              type: 'string',
+              defaultValue: 'somekingofdefaultvalue',
+            },
+          ],
+        },
+      ],
+    };
+    const result = await modelSchemaCollection.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
     res.send('Express + TypeScript Server');
   });
 
